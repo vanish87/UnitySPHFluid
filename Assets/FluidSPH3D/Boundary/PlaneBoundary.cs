65d35f307b28499cf40b29200df2ddfac953a65d
Yuan Li <vanish8.7@gmail.com>
2021-08-30 18:26:12 +0900

    update plane boundary

--- Assets/FluidSPH3D/Boundary/PlaneBoundary.cs
+++ Assets/FluidSPH3D/Boundary/PlaneBoundary.cs
@@ -5,32 +5,38 @@ using UnityTools.Common;
 
 namespace FluidSPH3D
 {
-	public interface IBoundarySampler
-	{
-		List<float3> Sample(float density = 1 / 32f);
-	}
-
 	[ExecuteInEditMode]
-	public class PlaneBoundary : MonoBehaviour, IBoundarySampler
+	public class PlaneBoundary : MonoBehaviour, IBoundary
 	{
-		//only sample xy plane
-		[SerializeField] protected Space3D plane = new Space3D();
+		public ISpace Space { get => this.data.space;}
+		[SerializeField] protected BoundaryConfigure.BData data = new BoundaryConfigure.BData();
+
+		public void Init(BoundaryConfigure.BData data)
+		{
+			this.data = data;
+			
+			var space = this.data.space;
+			this.gameObject.transform.localPosition = space.Center;
+			this.gameObject.transform.localRotation = space.Rotation;
+			this.gameObject.transform.localScale = space.Scale;
+		}
 
 		public List<float3> Sample(float density = 1 / 32f)
 		{
-			return Sampler.SampleXY(this.plane, 1, density);
+			//only sample xy plane
+			return Sampler.SampleXY(this.data.space, 1, density);
 		}
 
 		protected void Update()
 		{
-			this.plane.Center = this.gameObject.transform.localPosition;
-			this.plane.Rotation = this.gameObject.transform.localRotation;
-			this.plane.Scale = this.gameObject.transform.localScale;
+			this.data.space.Center = this.gameObject.transform.localPosition;
+			this.data.space.Rotation = this.gameObject.transform.localRotation;
+			this.data.space.Scale = this.gameObject.transform.localScale;
 		}
 
 		protected void OnDrawGizmos()
 		{
-			this.plane?.OnDrawGizmos();
+			this.data.space?.OnDrawGizmos();
 		}
 	}
 }

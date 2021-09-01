b49a0d982ff21630a2479c41ff6b2d5490db5450
Yuan Li <vanish8.7@gmail.com>
2021-09-01 17:53:34 +0900

    add weighted mesh sampler

--- Assets/FluidSPH3D/Boundary/Sampler.cs
+++ Assets/FluidSPH3D/Boundary/Sampler.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityTools.Debuging;
 
 namespace UnityTools.Common
 {
@@ -24,6 +25,93 @@ namespace UnityTools.Common
 					}
 				}
 			}
+			return ret;
+		}
+
+		public class WeightRandom
+		{
+			protected List<int> sum = new List<int>();
+			public WeightRandom(List<int> weights)
+			{
+				this.sum.Clear();
+				foreach(var i in Enumerable.Range(0, weights.Count))
+				{
+					LogTool.AssertIsTrue(weights[i] > 0);
+					var s = weights[i] + (i > 0 ? this.sum[i - 1] : 0);
+					this.sum.Add(s);
+				}
+			}
+
+			public int Random()
+			{
+				LogTool.AssertIsTrue(this.sum.Count > 0);
+
+				var total = this.sum.Last();
+				var rand = UnityEngine.Random.Range(0, total);
+
+				var l = 0;
+				var r = this.sum.Count;
+				while(l < r)
+				{
+					var mid = l + (r - l) / 2;
+					if (this.sum[mid] < rand) l = mid + 1;
+					else r = mid;
+				}
+
+				return r;
+			}
+
+		}
+		static public List<float3> SampleMeshSurface(Mesh mesh, int sampleNum = 1024)
+		{
+			var ret = new List<float3>();
+
+			if(mesh == null || mesh.GetTopology(0) != MeshTopology.Triangles)
+			{
+				LogTool.Log("Invalid mesh", LogLevel.Warning);
+				return ret;
+			}
+
+			var triangles = mesh.triangles;
+
+			var area = new List<float>();
+			var minArea = float.MaxValue;
+
+			foreach (var idx in Enumerable.Range(0, triangles.Length / 3))
+			{
+				var pa = mesh.vertices[triangles[idx*3]];
+				var pb = mesh.vertices[triangles[idx*3+1]];
+				var pc = mesh.vertices[triangles[idx*3+2]];
+				var A = pa-pc;
+				var B = pb-pc;
+				var a = 0.5f * math.length(math.cross(A, B));
+				area.Add(a);
+
+				minArea = math.min(minArea, a);
+			}
+
+			foreach (var i in Enumerable.Range(0, area.Count)) area[i] /= minArea;
+
+			var weights = area.Select(v=>Mathf.CeilToInt(v)).ToList();
+			var rand = new WeightRandom(weights);
+
+			while(sampleNum-->0)
+			{
+				var idx = rand.Random();
+
+				var pa = mesh.vertices[triangles[idx*3]];
+				var pb = mesh.vertices[triangles[idx*3+1]];
+				var pc = mesh.vertices[triangles[idx*3+2]];
+
+				var u = UnityEngine.Random.value;
+				var v = UnityEngine.Random.value;
+				if (u + v > 1) { u = 1 - u; v = 1 - v; }
+
+				var p = u * pa + v * pb + (1 - (u + v)) * pc;
+
+				ret.Add(p);
+			}
+
 			return ret;
 		}
 

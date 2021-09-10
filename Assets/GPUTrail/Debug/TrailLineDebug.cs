3a3b255ae6834d7478dc69f3bccdd5701a57b1c3
Yuan Li <vanish8.7@gmail.com>
2021-09-10 14:00:56 +0900

    refine cpu trail code

--- Assets/GPUTrail/Debug/TrailLineDebug.cs
+++ Assets/GPUTrail/Debug/TrailLineDebug.cs
@@ -8,55 +8,55 @@ using UnityTools.Common;
 
 namespace GPUTrail
 {
-    [ExecuteInEditMode]
+	[ExecuteInEditMode]
 	public class TrailLineDebug : MonoBehaviour
 	{
-        [SerializeField] protected List<float4> trailPos = new List<float4>();
-        [SerializeField] protected Shader shader;
-        [SerializeField] protected float thiness = 0.1f;
-        [SerializeField] protected float miterLimit = 0.75f;
-        [SerializeField] protected bool zScale = true;
-        protected DisposableMaterial material;
-		protected GPUBufferVariable<float4> trailGPUData = new GPUBufferVariable<float4>();
+		[System.Serializable]
+		public class TrailData : GPUContainer
+		{
+			[Shader(Name = "_Thiness")] public float thiness = 0.1f;
+			[Shader(Name = "_MiterLimit")] public float miterLimit = 0.75f;
+			[Shader(Name = "_zScale")] public bool zScale = true;
+			[Shader(Name = "_TrailData")] public GPUBufferVariable<float4> trailGPUData = new GPUBufferVariable<float4>();
+		}
+		[SerializeField] protected List<float4> trailPos = new List<float4>();
+		[SerializeField] protected Shader shader;
+		[SerializeField] protected TrailData trailData = new TrailData();
+		protected DisposableMaterial material;
 
-        protected void UpdateBuffer()
-        {
-            if(this.trailPos.Count != this.trailGPUData.Size) this.trailGPUData.InitBuffer(this.trailPos.Count, true);
-            foreach(var i in Enumerable.Range(0, this.trailGPUData.Size))
-            {
-                this.trailGPUData.CPUData[i] = this.trailPos[i];
-            }
-            this.trailGPUData.SetToGPUBuffer();
-        }
-        protected void OnEnable()
-        {
-            this.material = new DisposableMaterial(this.shader);
-        }
-        protected void OnDisable()
-        {
-            this.material?.Dispose();
-            this.trailGPUData?.Release();
-        }
+		protected void UpdateBuffer()
+		{
+			if (this.trailPos.Count != this.trailData.trailGPUData.Size) this.trailData.trailGPUData.InitBuffer(this.trailPos.Count, true);
+			foreach (var i in Enumerable.Range(0, this.trailData.trailGPUData.Size))
+			{
+				this.trailData.trailGPUData.CPUData[i] = this.trailPos[i];
+			}
+		}
+		protected void OnEnable()
+		{
+			this.material = new DisposableMaterial(this.shader);
+		}
+		protected void OnDisable()
+		{
+			this.material?.Dispose();
+			this.trailData?.Release();
+		}
 
-        protected void Update()
-        {
-            this.UpdateBuffer();
+		protected void Update()
+		{
+			this.UpdateBuffer();
 
-            Material mat = this.material;
-            mat.SetBuffer("_TrailData", this.trailGPUData);
-            mat.SetInt("_TrailDataCount", this.trailGPUData.Size);
-            mat.SetFloat("_Thiness", this.thiness);
-            mat.SetFloat("_MiterLimit", this.miterLimit);
-            mat.SetInt("_zScale", this.zScale?1:0);
+			Material mat = this.material;
+			this.trailData.UpdateGPU(mat);
 			var b = new Bounds(Vector3.zero, Vector3.one * 10000);
-			Graphics.DrawProcedural(material, b, MeshTopology.Points, this.trailGPUData.Size);
-        }
-        protected void OnDrawGizmos()
-        {
-            foreach(var p in this.trailPos)
-            {
+			Graphics.DrawProcedural(material, b, MeshTopology.Points, this.trailData.trailGPUData.Size);
+		}
+		protected void OnDrawGizmos()
+		{
+			foreach (var p in this.trailPos)
+			{
 				Gizmos.DrawWireSphere(new Vector3(p.x, p.y, p.z), 0.1f);
-            }
-        }
+			}
+		}
 	}
 }

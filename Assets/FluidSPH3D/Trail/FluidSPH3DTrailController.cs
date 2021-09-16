7ab01e0d37d9c69e761b4788dee180ce65936efd
Yuan Li <vanish8.7@gmail.com>
2021-09-16 16:49:27 +0900

    add override init for fluid

--- Assets/FluidSPH3D/Trail/FluidSPH3DTrailController.cs
+++ Assets/FluidSPH3D/Trail/FluidSPH3DTrailController.cs
@@ -5,6 +5,17 @@ namespace FluidSPH3D
 {
 	public class FluidSPH3DTrailController: GPUTrailController<TrailParticle>
 	{
-
+		public override void Init()
+		{
+			base.Init();
+		}
+		public override void Deinit()
+		{
+			base.Deinit();
+		}
+		protected override void Update()
+		{
+			base.Update();
+		}
 	}
 }
